6ec9fbabb1b63ebeedaeb7a4fb1f261dc56d0b01
stanislavruhala <stanislav.ya.ruhala@ukd.edu.ua>
2025-10-21 10:34:30 +0300

    Update and rename README.md to matrixsort.js

---
+++ matrixsort.js
@@ -0,0 +1,13 @@
+const numbers = [
+  [45,16,57],
+  [43,55,56],
+  [74,18,91],
+];
+
+for (let maks = 0; maks < numbers.length; maks++) {
+  for (let start = 0; start < numbers[maks].length; start++) {
+
+
+    console.log(`${maks + 1} ${start + 1} = ${numbers[maks][start]}`);
+  }
+}# MatrixSort
